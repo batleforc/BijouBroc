dbf6305e76fac76fa420d318f4748e071d0eb999
Maxime Leriche <maxleriche.60@gmail.com>
2021-06-26 12:34:04 +0200

    [add] axios, bulma

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+require("@/assets/main.sass")
 import "./registerServiceWorker";
 
 createApp(App).use(store).use(router).mount("#app");
---
+++ src/views/test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column">First column</div>
+      <div class="column">Second column</div>
+      <div class="column">Third column</div>
+      <div class="column">Fourth column</div>
+    </div>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+  data() {
+    return {
+      message: "patrick",
+    };
+  },
+};
+</script>
